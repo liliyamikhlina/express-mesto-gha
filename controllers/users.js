7b757075c516a8e20016dee2338f6f40805468c5
Liliya Mikhlina <liliya.mikhlina@yandex.ru>
2023-12-18 20:39:18 +0200

    fix users route

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,30 @@ module.exports.getUsers = (req, res) => {
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports.getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const {
+    name, about, avatar,
+  } = req.body;
+
+  User.create({
+    name, about, avatar,
+  })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getUsers } = require('../controllers/users');
+const { getUsers, getUserById, createUser } = require('../controllers/users');
 
 router.get('/', getUsers);
+router.get('/:userId', getUserById);
+router.post('/', createUser);
 
 module.exports = router;
