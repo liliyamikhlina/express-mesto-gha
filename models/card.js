a13696a57fb1a5ebabad367f087b7a9516d3323e
Liliya Mikhlina <liliya.mikhlina@yandex.ru>
2023-12-19 15:11:03 +0200

    feat: updateUser, updateAvatar, likeCard, dislikeCard

--- models/card.js
+++ models/card.js
@@ -16,6 +16,15 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('card', cardSchema);
