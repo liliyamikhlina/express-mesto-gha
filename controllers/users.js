566b58ed496fbbf6b03636b413a22545abcc2b9f
Liliya Mikhlina <liliya.mikhlina@yandex.ru>
2024-01-07 14:14:15 +0200

    before 14 sprint

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -13,41 +12,30 @@ module.exports.getUserById = (req, res) => {
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        return res
-          .status(404)
-          .send({ message: "Пользователь с указанным _id не найден" });
+        return res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
       }
       return res.send({ data: user });
     })
     .catch((err) => {
-      if (err.name === "CastError") {
-        return res
-          .status(400)
-          .send({ message: "Передан некорректный _id пользователя" });
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Передан некорректный _id пользователя' });
       }
       return res.status(500).send({ message: err.message });
     });
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar, email, password } = req.body;
-  bcrypt
-    .hash(password, 10)
-    .then((hash) =>
-      User.create({
-        name,
-        about,
-        avatar,
-        email,
-        password: hash,
-      })
-    )
+  const {
+    name, about, avatar,
+  } = req.body;
+
+  User.create({
+    name, about, avatar,
+  })
     .then((user) => res.status(201).json({ data: user }))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        return res.status(400).send({
-          message: "Переданы некорректные данные при создании пользователя",
-        });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
       }
       return res.status(500).send({ message: err.message });
     });
@@ -55,24 +43,16 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true }
-  )
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res
-          .status(404)
-          .send({ message: "Пользователь с указанным _id не найден" });
+        return res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
       }
       return res.send({ data: user });
     })
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        return res.status(400).send({
-          message: "Переданы некорректные данные при обновлении профиля",
-        });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
       }
       return res.status(500).send({ message: err.message });
     });
@@ -80,65 +60,26 @@ module.exports.updateUser = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true }
-  )
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res
-          .status(404)
-          .send({ message: "Пользователь с указанным _id не найден" });
+        return res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
       }
       return res.send({ data: user });
     })
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        return res
-          .status(400)
-          .send({ message: "Переданы некорректные данные при обновлении " });
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении ' });
       }
       return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports.login = (req, res) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).send({ message: "Incorrect email or password" });
-      }
-      bcrypt
-        .compare(password, user.password)
-        .then((passwordMatch) => {
-          if (!passwordMatch) {
-            return res
-              .status(401)
-              .send({ message: "Incorrect email or password" });
-          }
-
-          const token = jwt.sign({ _id: user._id }, "your-secret-key", {
-            expiresIn: "1w",
-          });
-
-          res.cookie("jwt", token, {
-            httpOnly: true,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-          });
-
-
-          res.status(200).send({ message: "Login successful" });
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send({ message: "Internal Server Error" });
-        });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send({ message: "Internal Server Error" });
-    });
-};
+// module.exports.createUser = (req, res) => {
+//   User.create({
+//     email: req.body.email,
+//     password: req.body.password,
+//   })
+//     .then((user) => res.send(user))
+//     .catch((err) => res.status(400).send(err));
+// };
--- models/user.js
+++ models/user.js
@@ -4,36 +4,32 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
+    required: [true, 'Поле "about" должно быть заполнено'],
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    required: [true, 'Поле "avatar" должно быть заполнено'],
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
     },
     email: {
       type: String,
-      required: [true, 'Поле "email" должно быть заполнено'],
+      required: true,
       unique: true,
-      validate: {
-        validator: (v) => validator.isEmail(v),
-        message: 'Некорректный email',
-      },
     },
     password: {
       type: String,
-      required: [true, 'Поле "password" должно быть заполнено'],
-      minlength: [8, 'Минимальная длина поля "password" - 8'],
+      required: true,
+      minlength: 8,
     },
   },
 }, { versionKey: false });
