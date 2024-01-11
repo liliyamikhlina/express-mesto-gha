9043e6d1b334ab028eca4b92bef48d250c626f15
Liliya Mikhlina <liliya.mikhlina@yandex.ru>
2024-01-11 15:14:50 +0200

    fix: createCard link required

--- routes/cards.js
+++ routes/cards.js
@@ -9,7 +9,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().regex(/^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?/),
+    link: Joi.string().required().regex(/^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?/),
   }),
 }), createCard);
 
